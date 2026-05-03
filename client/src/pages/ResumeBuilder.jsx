7cdbedacad048a277687b0d7ef3b4a9437061e10
Pranjul Jaiswal <146024663+pranjuljais@users.noreply.github.com>
2026-05-03 17:47:28 +0530

    final

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -160,7 +160,7 @@ const ResumeBuilder = () => {
             <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
               <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
               <hr
-                className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000 "
+                className="absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-2000 "
                 style={{
                   width: `${(activeSectionIndex * 100) / (sections.length - 1)}%`,
                 }}
@@ -273,7 +273,7 @@ const ResumeBuilder = () => {
                 onClick={() => {
                   toast.promise(saveResume, { loading: "Saving..." });
                 }}
-                className="bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm"
+                className="bg-linear-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm"
               >
                 Save Changes
               </button>
